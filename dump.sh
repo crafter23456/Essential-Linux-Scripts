3289e3f3a67a246ce15c0f7abf36421c4ae44be8
crafter23456 <64259198+crafter23456@users.noreply.github.com>
2021-05-05 22:59:33 +0200

    add dump.sh

---
+++ dump.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+cd /backup
+echo -e "\n---------------------------------------------------------------------------------\n"
+echo -e "What database you want to dump?\n"
+
+
+files=$(ls *.sql)
+i=1
+
+for j in $files
+do
+echo "$i.$j"
+file[i]=$j
+i=$(( i + 1 ))
+done
+
+echo -e "\n---------------------------------------------------------------------------------\n"
+
+echo -e "Enter number\n"
+
+read input
+
+re='^[0-9]+$'
+
+while :; do
+if ! [[ $input =~ $re ]] ; then
+		if [ "$input" == "l" ] || [ "$input" == "exit" ] || [ "$input" == "q" ]; then
+			echo "This script will exit now."
+			exit 1
+		else
+			echo "Error: Not a number"
+			echo -e "Enter number"
+			read input
+		fi
+   
+else
+	break
+fi
+done
+
+echo -e "Enter databasen or 'all' if you want to dump all databases. \n"
+
+read database
+
+while :; do
+		if [ "$database" == "l" ] || [ "$database" == "exit" ] || [ "$database" == "q" ]; then
+			echo "This script will exit now."
+			exit 1
+		else
+			break
+		fi
+done
+
+read -r -p "Please insert 'YES' to allow the installation. Otherwise this script will exit here: " ANSWER
+
+if [ "$ANSWER" == "YES" ] || [ "$ANSWER" == "y" ]; then
+	if [ $database == "all" ]; then
+		echo "Dumping all database..."
+		mysql -p -o $database < ${file[$input]}
+	else
+		echo "Dumping database..."
+		mysql -p < ${file[$input]}
+	fi
+else
+	echo "Your answer was \"$ANSWER\" and not YES. So this script will exit now."
+	exit 1
+fi
